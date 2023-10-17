d148a0e5046c5af57ea3eedf8d4cd30e4d3f0a36
Austin Rooks <Austionian@users.noreply.github.com>
2023-10-16 21:02:39 -0500

    add api token to post requests

--- src/lib.rs
+++ src/lib.rs
@@ -21,27 +21,45 @@ pub async fn main(request: Request, env: Env, _ctx: Context) -> Result<Response>
                 None => Response::error("Not found", 404),
             }
         })
-        .post_async("/:id", |_, ctx| async move {
-            let id = ctx.param("id").unwrap();
-            let d1 = ctx.env.d1("DB")?;
-            let statement =
-                d1.prepare("UPDATE posts SET read_count = read_count + 1 WHERE id = ?1");
-            let query = statement.bind(&[id.into()])?;
-            let result = query.run().await;
-            match result {
-                Ok(_) => Response::ok("Row updated."),
-                Err(_) => Response::error("Row not updated", 401),
+        .post_async("/:id", |request, ctx| async move {
+            let key = request.headers().get("API_TOKEN");
+            match key {
+                Ok(Some(key)) => {
+                    if key != ctx.env.secret("API_TOKEN").unwrap().to_string() {
+                        return Response::error("Unauthorized", 403);
+                    }
+                    let id = ctx.param("id").unwrap();
+                    let d1 = ctx.env.d1("DB")?;
+                    let statement =
+                        d1.prepare("UPDATE posts SET read_count = read_count + 1 WHERE id = ?1");
+                    let query = statement.bind(&[id.into()])?;
+                    let result = query.run().await;
+                    match result {
+                        Ok(_) => Response::ok("Row updated."),
+                        Err(_) => Response::error("Row not updated", 401),
+                    }
+                }
+                Ok(None) | Err(_) => Response::error("Unauthorized", 403),
             }
         })
-        .post_async("/new/:id", |_, ctx| async move {
-            let id = ctx.param("id").unwrap();
-            let d1 = ctx.env.d1("DB")?;
-            let statement = d1.prepare("INSERT INTO posts (id, read_count) VALUES (?1, 0)");
-            let query = statement.bind(&[id.into()])?;
-            let result = query.run().await;
-            match result {
-                Ok(_) => Response::ok("Row created."),
-                Err(_) => Response::error("Row not created.", 500),
+        .post_async("/new/:id", |request, ctx| async move {
+            let key = request.headers().get("API_TOKEN");
+            match key {
+                Ok(Some(key)) => {
+                    if key != ctx.env.secret("API_TOKEN").unwrap().to_string() {
+                        return Response::error("Unauthorized", 403);
+                    }
+                    let id = ctx.param("id").unwrap();
+                    let d1 = ctx.env.d1("DB")?;
+                    let statement = d1.prepare("INSERT INTO posts (id, read_count) VALUES (?1, 0)");
+                    let query = statement.bind(&[id.into()])?;
+                    let result = query.run().await;
+                    match result {
+                        Ok(_) => Response::ok("Row created."),
+                        Err(_) => Response::error("Row not created.", 500),
+                    }
+                }
+                Ok(None) | Err(_) => Response::error("Unauthorized", 403),
             }
         })
         .run(request, env)
